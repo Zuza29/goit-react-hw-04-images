edbf1f0ab2f3e84aca64a9469eb1d9536a64bd0a
Zuzanna Kaźmierczak <zusapphire@gmail.com>
2023-03-23 22:31:23 +0100

    bugfix

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -3,30 +3,28 @@ import css from './Modal.module.css';
 import propTypes from 'prop-types';
 
 export const Modal = ({ image, onClose }) => {
- const handleClose = event => {
-    if (event.code === 'Escape') {
-      return onClose();
-    }
-  };
-
- useEffect(() => {
-  window.addEventListener('keydown', handleClose);
-  return () => { 
+  useEffect(() => {
+    const handleClose = event => {
+      if (event.code === 'Escape') {
+        return onClose();
+      }
+    };
+    window.addEventListener('keydown', handleClose);
+    return () => {
       window.removeEventListener('keydown', handleClose);
-     }
-  }, [])
+    };
+  }, [onClose]);
 
   // useEffect(() => { function that will execute }, [ condition on which the function will execute ])
 
-    return (
-      <div className={css.overlay} onClick={onClose}>
-        <div className={css.modal}>
-          <img src={image} alt="Result" />
-        </div>
+  return (
+    <div className={css.overlay} onClick={onClose}>
+      <div className={css.modal}>
+        <img src={image} alt="Result" />
       </div>
-    );
-  
-}
+    </div>
+  );
+};
 
 Modal.propTypes = {
   image: propTypes.string,
